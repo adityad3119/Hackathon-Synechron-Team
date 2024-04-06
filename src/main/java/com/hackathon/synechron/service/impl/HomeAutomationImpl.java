bd556179d89c92c0e98f1baa699fc71dab571f9f
adity <adityad3119@gmail.com>
2024-04-06 12:44:29 +0100

    day1

--- src/main/java/com/hackathon/synechron/service/impl/HomeAutomationImpl.java
+++ src/main/java/com/hackathon/synechron/service/impl/HomeAutomationImpl.java
@@ -1,8 +1,6 @@
 package com.hackathon.synechron.service.impl;
 
 import com.hackathon.synechron.service.HomeAutomation;
-import com.pi4j.Pi4J;
-import com.pi4j.platform.Platforms;
 
 public class HomeAutomationImpl implements HomeAutomation {
 
@@ -10,13 +8,7 @@ public class HomeAutomationImpl implements HomeAutomation {
 	public String turnLightOff() {
 		// TODO Auto-generated method stub
 		
-		var pi4j = Pi4J.newAutoContext();
-		Platforms platforms = pi4j.platforms();
 
-		console.box("Pi4J PLATFORMS");
-		console.println();
-		platforms.describe().print(System.out);
-		console.println();
 		return null;
 	}
 
